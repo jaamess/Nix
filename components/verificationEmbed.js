45497b2b3ce32315c689bb01210549985d6d6f90
Thiago <thiagozevedo3@gmail.com>
2024-05-17 20:59:38 -0300

    fixes sending verification embed

--- components/verificationEmbed.js
+++ components/verificationEmbed.js
@@ -1,97 +1,131 @@
-const { EmbedBuilder, ActionRowBuilder, UserSelectMenuBuilder, ButtonBuilder } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, UserSelectMenuBuilder, ButtonBuilder, InteractionType } = require('discord.js');
+const config = require('../config.json');
 
-async function sendVerificationEmbed(verificationChannel, client) {
-    // Verifica se o embed já foi enviado
-    const messages = await verificationChannel.messages.fetch({ limit: 100 });
-    const embedSent = messages.find(m =>
-        m.embeds.length > 0 &&
-        m.embeds[0].title === 'Verificação' &&
-        m.embeds[0].description === 'Quem você conhece no servidor?'
-    );
+async function sendVerificationEmbed(client) {
+    try {
+        const verificationChannel = await client.channels.fetch(config.settings.testGuild.verificationChannel);
+        const adminChannel = await client.channels.fetch(config.settings.testGuild.adminChannel);
+        const verifiedRoleID = config.settings.verifiedRole;
 
-    // Se o embed já está no canal, não envia novamente
-    if (embedSent) {
-        console.log('O embed de verificação já está no canal.');
-        return;
-    }
+        // Verifica se o embed já foi enviado
+        const messages = await verificationChannel.messages.fetch({ limit: 100 });
+        const embedSent = messages.find(m =>
+            m.embeds.length > 0 &&
+            m.embeds[0].title === 'Verificação' &&
+            m.embeds[0].description.includes('Quem você conhece no servidor?')
+        );
 
-    // Cria o embed de verificação
-    let embed = new EmbedBuilder()
-        .setTitle('Verificação')
-        .setDescription('Quem você conhece no servidor?')
-        .setColor('#fd0e35');
-
-    // Cria o menu de seleção de usuário
-    const userSelectMenu = new UserSelectMenuBuilder()
-        .setCustomId('send-verification')
-        .setPlaceholder('Selecione ou digite um nickname.')
-        .setMaxValues(1)
-        .setMinValues(1);
-
-    // Cria a actionRow com o menu de seleção de usuário
-    const row = new ActionRowBuilder()
-        .addComponents(userSelectMenu);
-
-    // Envia o embed com o menu de seleção de usuário para o canal de verificação
-    const message = await verificationChannel.send({
-        embeds: [embed],
-        components: [row]
-    });
-
-    // Filtro do coletor de mensagens
-    const collectorFilter = c => c.user.id === client.user.id;
-
-    // Cria um coletor de componentes de mensagem
-    const collector = verificationChannel.createMessageComponentCollector({
-        filter: collectorFilter,
-        time: 10_000
-    });
-
-    // Evento do coletor
-    collector.on('collect', async (c) => {
-        if (!c.values) return;
-
-        let userID = c.values[0];
-        let user = await client.users.fetch(userID);
-
-        // Se o usuário não for encontrado, retorna
-        if (!user) {
-            return console.log('Usuário não encontrado.');
+        // Se o embed já está no canal, não envia novamente
+        if (embedSent) {
+            return;
         }
 
         // Cria o embed de verificação
-        const verifyEmbed = new EmbedBuilder()
+        let embed = new EmbedBuilder()
             .setTitle('Verificação')
-            .setDescription(`Usuário: ${user.username}`)
-            .addFields({
-                name: 'Quem conhece?',
-                value: `<@${user.id}> (${user.username})`,
-                inline: true
-            })
-            .setColor('#ffffff');
+            .setDescription('Quem você conhece no servidor?')
+            .setColor('#fd0e35');
 
-        // Cria botões para aprovação ou reprovação
-        let option1 = new ButtonBuilder()
-            .setStyle('Success')
-            .setLabel('Aprovar')
-            .setCustomId(`aprovar-${user.id}`);
+        // Cria o menu de seleção de usuário
+        const userSelectMenu = new UserSelectMenuBuilder()
+            .setCustomId('send-verification')
+            .setPlaceholder('Selecione ou digite um nickname.')
+            .setMaxValues(1)
+            .setMinValues(1);
 
-        let option2 = new ButtonBuilder()
-            .setStyle('Danger')
-            .setLabel('Reprovar')
-            .setCustomId(`reprovar-${user.id}`);
-
-        // Cria a linha de ação com os botões
-        const row2 = new ActionRowBuilder()
-            .addComponents([option1, option2]);
-
-        // Envia o embed de verificação com botões para o canal de verificação
-        verificationChannel.send({
-            content: `<@${user.id}> - ${user.toString()}`,
-            embeds: [verifyEmbed],
-            components: [row2]
+        // Adiciona os usuários ao menu de seleção
+        const guildMembers = await verificationChannel.guild.members.fetch();
+        guildMembers.forEach(member => {
+            if (!member.user.bot) {
+                userSelectMenu.addOptions({
+                    label: member.user.username,
+                    value: member.user.id
+                });
+            }
         });
-    });
+
+        // Cria a actionRow com o menu de seleção de usuário
+        const row = new ActionRowBuilder().addComponents(userSelectMenu);
+
+        // Envia o embed com o menu de seleção de usuário para o canal de verificação
+        await verificationChannel.send({
+            embeds: [embed],
+            components: [row]
+        });
+
+        // Cria um coletor de componentes de mensagem
+        const collector = verificationChannel.createMessageComponentCollector({
+            time: 15000 // 15 segundos para interação
+        });
+
+        collector.on('collect', async interaction => {
+            if (interaction.type !== InteractionType.MessageComponent) return;
+
+            // Verifica se a interação é do menu de seleção
+            if (interaction.customId === 'send-verification') {
+                await interaction.reply({ content: 'Sua solicitação de verificação foi enviada!', ephemeral: true });
+
+                let userID = interaction.values[0];
+                let user = await client.users.fetch(userID);
+
+                // Se o usuário não for encontrado, retorna
+                if (!user) {
+                    return;
+                }
+
+                // Cria o embed de verificação para o canal de administração
+                const verifyEmbed = new EmbedBuilder()
+                    .setTitle('Verificação')
+                    .setDescription(`Usuário: ${user.username}`)
+                    .addFields({
+                        name: 'Quem conhece?',
+                        value: `<@${user.id}> (${user.username})`,
+                        inline: true
+                    })
+                    .setColor('#ffffff');
+
+                // Cria botões para aprovação ou reprovação
+                let approveButton = new ButtonBuilder()
+                    .setStyle('Success')
+                    .setLabel('Aprovar')
+                    .setCustomId(`approve-${user.id}`);
+
+                let denyButton = new ButtonBuilder()
+                    .setStyle('Danger')
+                    .setLabel('Reprovar')
+                    .setCustomId(`deny-${user.id}`);
+
+                // Cria a linha de ação com os botões
+                const row2 = new ActionRowBuilder().addComponents([approveButton, denyButton]);
+
+                // Envia o embed de verificação com botões para o canal de administração
+                await adminChannel.send({
+                    content: `<@${user.id}> - ${user.toString()}`,
+                    embeds: [verifyEmbed],
+                    components: [row2]
+                });
+
+                // Coletor para os botões de aprovação/reprovação
+                const buttonCollector = adminChannel.createMessageComponentCollector({
+                    componentType: 'BUTTON',
+                    time: 15000 // 15 segundos para interação
+                });
+
+                buttonCollector.on('collect', async buttonInteraction => {
+                    if (buttonInteraction.customId === `approve-${user.id}`) {
+                        const member = await interaction.guild.members.fetch(userID);
+                        await member.roles.add(verifiedRoleID);
+                        await buttonInteraction.reply({ content: `O usuário <@${user.id}> foi aprovado e verificado com sucesso.`, ephemeral: true });
+                    } else if (buttonInteraction.customId === `deny-${user.id}`) {
+                        await user.send('Sua solicitação de verificação foi negada.');
+                        await buttonInteraction.reply({ content: `O usuário <@${user.id}> foi reprovado.`, ephemeral: true });
+                    }
+                });
+            }
+        });
+    } catch (error) {
+        console.error('Ocorreu um erro em sendVerificationEmbed:', error);
+    }
 }
 
 module.exports = { sendVerificationEmbed };
--- main.js
+++ main.js
@@ -2,83 +2,63 @@ const { promises: fs } = require('fs');
 const path = require('path');
 const { Client: DiscordClient, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const { sendVerificationEmbed } = require('./components/verificationEmbed.js');
 
-// Classe que estende o cliente padrão do Discord
 class Client extends DiscordClient {
     constructor(options) {
         super(options);
-        // Coleção para armazenar os comandos
         this.commands = new Collection();
     }
 }
 
 async function main() {
-    // Cria uma nova instância do cliente com as intenções necessárias
-    const client = new Client({ intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMessageReactions
-    ]});
+    const client = new Client({
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.GuildMessageReactions
+        ]
+    });
 
-    // Evento que é executado uma vez quando o cliente está pronto
     client.once('ready', async () => {
-        console.log(`Logado como ${client.user.tag}!`);
-        
-        // Caminho para a pasta de comandos
+        console.log(`Logged in as ${client.user.tag}!`);
+
         const foldersPath = path.join(__dirname, 'commands');
-        // Lê os diretórios de comandos
         const commandFolders = await fs.readdir(foldersPath);
 
-        // Itera sobre cada pasta de comandos
         for (const folder of commandFolders) {
-            // Caminho para os arquivos de comando dentro da pasta
             const commandsPath = path.join(foldersPath, folder);
-            // Filtra e lê os arquivos de comando que terminam com '.js'
             const commandFiles = (await fs.readdir(commandsPath)).filter(file => file.endsWith('.js'));
-            // Itera sobre cada arquivo de comando
             for (const file of commandFiles) {
-                // Caminho completo para o arquivo de comando
                 const filePath = path.join(commandsPath, file);
-                // Requer o arquivo de comando
                 const command = require(filePath);
-                // Verifica se o comando tem as propriedades 'data' e 'execute'
                 if ('data' in command && 'execute' in command) {
-                    // Vincula o método execute à instância do cliente
                     command.execute = command.execute.bind(client);
-                    // Define o comando na coleção de comandos
                     client.commands.set(command.data.name, command);
                 } else {
-                    // Da um aviso se o comando estiver faltando as propriedades necessárias
-                    console.log(`[AVISO] O comando em ${filePath} está faltando uma propriedade "data" ou "execute" necessária.`);
+                    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
                 }
             }
-        }              
+        }
 
-        // Caminho para a pasta de eventos
         const eventsPath = path.join(__dirname, 'events');
-        // Filtra e lê os arquivos de evento que terminam com '.js'
         const eventFiles = (await fs.readdir(eventsPath)).filter(file => file.endsWith('.js'));
 
-        // Itera sobre cada arquivo de evento
         for (const file of eventFiles) {
-            // Caminho completo para o arquivo de evento
             const filePath = path.join(eventsPath, file);
-            // Requer o arquivo de evento
             const event = require(filePath);
-            // Verifica se o evento deve ser executado uma vez ou várias vezes
             if (event.once) {
-                // Vincula o evento para ser executado uma vez
                 client.once(event.name, (...args) => event.execute.apply(event, args));
             } else {
-                // Vincula o evento para ser executado várias vezes
                 client.on(event.name, (...args) => event.execute.apply(event, args));
             }
         }
+
+        // Call the verification function
+        await sendVerificationEmbed(client);
     });
 
-    // Faz login no cliente com o token
-    client.login(token);
+    client.login(token).catch(console.error);
 }
 
-// Executa a função principal e joga os erros no console
 main().catch(console.error);
